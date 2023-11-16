ad110c4d060e08b72da609962decadfc5315274e
extmarcelosouza <131166422+extmarcelosouza@users.noreply.github.com>
2023-11-16 13:11:45 -0300

    Delete api-cnh directory

--- api-cnh/codecommit.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_codecommit_repository" "api-cnh" {
-  repository_name = "api-cnh"
-  description     = "api-cnh repository"
-
-  tags = {
-    name = "api-cnh"
-  }
-  default_branch = "main"
-  }
